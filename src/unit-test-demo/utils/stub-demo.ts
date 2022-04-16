61b264ad9f18fbe448ff38f301f92027ea1a63ba
ningrunting <lavendernrt@gmail.com>
2022-04-16 16:42:39 +0800

    add sinon stub demo to stub method in file

--- src/unit-test-demo/utils/stub-demo.ts
+++ src/unit-test-demo/utils/stub-demo.ts
@@ -1,9 +1,17 @@
-import { isMobileDevice } from '@/unit-test-demo/utils/device'
+import { isLandscape, isMobileDevice } from '@/unit-test-demo/utils/device'
 
 export const getMobileLink = () => {
   if (isMobileDevice) {
     return 'https://mobile.address'
   } else {
     return 'https://pc.address'
+  }
+}
+
+export const useTemplate = () => {
+  if (isLandscape()) {
+    return 'templateA'
+  } else {
+    return 'templateB'
   }
 }
--- tests/unit/utils/stub-demo.spec.ts
+++ tests/unit/utils/stub-demo.spec.ts
@@ -1,10 +1,10 @@
-import { getMobileLink } from '@/unit-test-demo/utils/stub-demo'
+import { getMobileLink, useTemplate } from '@/unit-test-demo/utils/stub-demo'
 import * as device from '@/unit-test-demo/utils/device'
 import sinon from 'sinon'
 // getMobileLink调用了isMobileDevice来判断是否是mobile,如果需要测试两个逻辑分支，需要stub两种场景的返回值
 // 知识点：stub 一个文件里的某一个变量
 
-describe('link getMobileLink', function () {
+describe('stub value getMobileLink', function () {
   it('should return mobile link when on mobile', function () {
     sinon.stub(device, 'isMobileDevice').value(true)
     expect(getMobileLink()).toEqual('https://mobile.address')
@@ -12,5 +12,19 @@ describe('link getMobileLink', function () {
   it('should return pc link when not on mobile', function () {
     sinon.stub(device, 'isMobileDevice').value(false)
     expect(getMobileLink()).toEqual('https://pc.address')
+  })
+})
+
+describe('stub method useTemplate', function () {
+  afterEach(() => {
+    (device.isLandscape as any).restore()
+  })
+  it('should return templateA when is landscape', function () {
+    sinon.stub(device, 'isLandscape').returns(true)
+    expect(useTemplate()).toEqual('templateA')
+  })
+  it('should return templateB when is priorat', function () {
+    sinon.stub(device, 'isLandscape').returns(false)
+    expect(useTemplate()).toEqual('templateB')
   })
 })
