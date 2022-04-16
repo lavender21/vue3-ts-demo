c676b9c7153d35a15c62bf1d9e36813973f2c698
ningrunting <lavendernrt@gmail.com>
2022-04-16 17:11:52 +0800

    add jest stub demo to stub method in file

--- tests/unit/utils/stub-demo.spec.ts
+++ tests/unit/utils/stub-demo.spec.ts
@@ -1,8 +1,9 @@
 import { getMobileLink, useTemplate } from '@/unit-test-demo/utils/stub-demo'
 import * as device from '@/unit-test-demo/utils/device'
 import sinon from 'sinon'
+import { isLandscape } from '@/unit-test-demo/utils/device'
 // getMobileLink调用了isMobileDevice来判断是否是mobile,如果需要测试两个逻辑分支，需要stub两种场景的返回值
-// 知识点：stub 一个文件里的某一个变量
+// stub 一个文件里的某一个变量 sinon stub jest目前不改变业务代码的前提下没有找到解决方案
 
 describe('stub value getMobileLink', function () {
   it('should return mobile link when on mobile', function () {
@@ -15,7 +16,8 @@ describe('stub value getMobileLink', function () {
   })
 })
 
-describe('stub method useTemplate', function () {
+// stub 一个文件里的某一个方法
+describe('stub method useTemplate use sinon', function () {
   afterEach(() => {
     (device.isLandscape as any).restore()
   })
@@ -25,6 +27,19 @@ describe('stub method useTemplate', function () {
   })
   it('should return templateB when is priorat', function () {
     sinon.stub(device, 'isLandscape').returns(false)
+    expect(useTemplate()).toEqual('templateB')
+  })
+})
+describe('stub method useTemplate use jest', function () {
+  beforeEach(() => {
+    jest.restoreAllMocks()
+  })
+  it('should return templateA when is landscape', function () {
+    jest.spyOn(device, 'isLandscape').mockReturnValue(true)
+    expect(useTemplate()).toEqual('templateA')
+  })
+  it('should return templateB when is priorat', function () {
+    jest.spyOn(device, 'isLandscape').mockReturnValue(false)
     expect(useTemplate()).toEqual('templateB')
   })
 })
